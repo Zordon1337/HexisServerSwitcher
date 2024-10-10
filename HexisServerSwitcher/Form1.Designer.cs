c950fa0faa361ed154422d0b989ac842ace1385d
ZRD <65111609+Zordon1337@users.noreply.github.com>
2024-10-10 12:49:43 +0200

    Dodaj pliki projektów.

---
+++ HexisServerSwitcher/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace HexisServerSwitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ApplyBtn = new System.Windows.Forms.Button();
+            this.IRC_SERVER = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GAME_SERVER = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SCORE_SERVER = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.WEB_SITE = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CRASH_REPORT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ApplyBtn
+            // 
+            this.ApplyBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(76)))), ((int)(((byte)(170)))));
+            this.ApplyBtn.FlatAppearance.BorderSize = 0;
+            this.ApplyBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ApplyBtn.ForeColor = System.Drawing.Color.White;
+            this.ApplyBtn.Location = new System.Drawing.Point(12, 187);
+            this.ApplyBtn.Name = "ApplyBtn";
+            this.ApplyBtn.Size = new System.Drawing.Size(325, 41);
+            this.ApplyBtn.TabIndex = 0;
+            this.ApplyBtn.Text = "Apply";
+            this.ApplyBtn.UseVisualStyleBackColor = false;
+            this.ApplyBtn.Click += new System.EventHandler(this.ApplyBtn_Click);
+            // 
+            // IRC_SERVER
+            // 
+            this.IRC_SERVER.Location = new System.Drawing.Point(92, 34);
+            this.IRC_SERVER.Name = "IRC_SERVER";
+            this.IRC_SERVER.Size = new System.Drawing.Size(246, 20);
+            this.IRC_SERVER.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 28);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HexisServerSwitcher";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IRC Server";
+            // 
+            // GAME_SERVER
+            // 
+            this.GAME_SERVER.Location = new System.Drawing.Point(92, 60);
+            this.GAME_SERVER.Name = "GAME_SERVER";
+            this.GAME_SERVER.Size = new System.Drawing.Size(246, 20);
+            this.GAME_SERVER.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(1, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Game Server";
+            // 
+            // SCORE_SERVER
+            // 
+            this.SCORE_SERVER.Location = new System.Drawing.Point(92, 86);
+            this.SCORE_SERVER.Name = "SCORE_SERVER";
+            this.SCORE_SERVER.Size = new System.Drawing.Size(246, 20);
+            this.SCORE_SERVER.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Score Server";
+            // 
+            // WEB_SITE
+            // 
+            this.WEB_SITE.Location = new System.Drawing.Point(92, 112);
+            this.WEB_SITE.Name = "WEB_SITE";
+            this.WEB_SITE.Size = new System.Drawing.Size(246, 20);
+            this.WEB_SITE.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(32, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Website";
+            // 
+            // CRASH_REPORT
+            // 
+            this.CRASH_REPORT.Location = new System.Drawing.Point(92, 138);
+            this.CRASH_REPORT.Name = "CRASH_REPORT";
+            this.CRASH_REPORT.Size = new System.Drawing.Size(246, 20);
+            this.CRASH_REPORT.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(3, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Crash Report";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(87)))));
+            this.ClientSize = new System.Drawing.Size(350, 240);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CRASH_REPORT);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.WEB_SITE);
+            this.Controls.Add(this.SCORE_SERVER);
+            this.Controls.Add(this.GAME_SERVER);
+            this.Controls.Add(this.IRC_SERVER);
+            this.Controls.Add(this.ApplyBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ApplyBtn;
+        private System.Windows.Forms.TextBox IRC_SERVER;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox GAME_SERVER;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SCORE_SERVER;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox WEB_SITE;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CRASH_REPORT;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ HexisServerSwitcher/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HexisServerSwitcher
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IRC_SERVER.Text = HostsUtils.GetCurrentValue("irc.dienay.net");
+            WEB_SITE.Text = HostsUtils.GetCurrentValue("hexis.dienay.net");
+            SCORE_SERVER.Text = HostsUtils.GetCurrentValue("hscore.dienay.net");
+            GAME_SERVER.Text = HostsUtils.GetCurrentValue("hnet.dienay.net");
+            CRASH_REPORT.Text = HostsUtils.GetCurrentValue("hcrash.dienay.net");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://zrd.ovh");
+        }
+
+        private void ApplyBtn_Click(object sender, EventArgs e)
+        {
+            HostsUtils.SetValue("irc.dienay.net", IRC_SERVER.Text);
+            HostsUtils.SetValue("hexis.dienay.net", WEB_SITE.Text);
+            HostsUtils.SetValue("hscore.dienay.net", SCORE_SERVER.Text);
+            HostsUtils.SetValue("hnet.dienay.net", GAME_SERVER.Text);
+            HostsUtils.SetValue("hcrash.dienay.net", CRASH_REPORT.Text);
+        }
+    }
+    public static class HostsUtils
+    {
+        private static string _hosts = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+        public static string GetCurrentValue(string key)
+        {
+            var lines = System.IO.File.ReadAllLines(_hosts);
+            foreach (var line in lines)
+            {
+                if (line.Contains(" "))
+                {
+                    var key1 = line.Split(' ')[0];
+                    var value1 = line.Split(' ')[1];
+                    if(key1 == value1)
+                    {
+                        if (key1.Equals(key))
+                            return "default";
+                    } else
+                    {
+                        if (key1.Equals(key))
+                            return value1;
+                    }
+                }
+            }
+            return key;
+        }
+        public static void SetValue(string key, string value)
+        {
+
+            if (GetCurrentValue(key) == key) // aka not found
+            {
+
+                var file = System.IO.File.ReadAllText(_hosts);
+                file += $"\n{key} {value}";
+                System.IO.File.WriteAllText(_hosts, file);
+            }
+            else
+            {
+                var lines = System.IO.File.ReadAllLines(_hosts);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains(" "))
+                    {
+                        var key1 = lines[i].Split(' ')[0];
+                        var value1 = lines[i].Split(' ')[1];
+                        if (key1.Equals(key))
+                        {
+                            lines[i] = $"{key} {value}";
+                        }
+                    }
+                }
+                System.IO.File.WriteAllLines(_hosts, lines);
+            }
+        }
+    }
+}
